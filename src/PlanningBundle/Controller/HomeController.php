9d4706e85f89ed66084a6d5f715d1846e8ee3f8f
benImran <benchaa.said-medjahed@hetic.net>
2017-10-19 11:33:57 +0200

    gulpinstall

---
+++ src/PlanningBundle/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PlanningBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('pages/home.html.twig');
+    }
+}
