fd5928f246a4a6f722252eca4fd4f64264d524d8
benImran <benchaa.said-medjahed@hetic.net>
2017-11-27 09:48:26 +0100

    Installation FulCalendar.JS

---
+++ src/PlanningBundle/Entity/CalendarEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace PlanningBundle\Entity;
+
+
+use AncaRebeca\FullCalendarBundle\Model\FullCalendarEvent;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Calend
+ *
+ * @ORM\Table(name="calendar_event")
+ * @ORM\Entity(repositoryClass="PlanningBundle\Repository\CalendarEventRepository")
+ */
+class CalendarEvent extends FullCalendarEvent
+{
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        // TODO: Implement toArray() method.
+    }
+}
