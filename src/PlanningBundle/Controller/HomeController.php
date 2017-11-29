c876d8c15e04d0e182047e96daab312d56de2297
benImran <benchaa.said-medjahed@hetic.net>
2017-11-29 09:22:09 +0100

    Twig pages

--- src/PlanningBundle/Controller/HomeController.php
+++ src/PlanningBundle/Controller/HomeController.php
@@ -8,10 +8,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller
 {
     /**
-     * @Route("/popo")
+     * @Route("/popo", name="premiere-page")
      */
     public function indexAction()
     {
-        return $this->render('PlanningBundle:Default:index.html.twig');
+        return $this->render('pages/page-acceuil.html.twig');
+    }
+
+    /**
+     * @Route("/planification", name="planifier-une-commande")
+     */
+    public function planificationAction()
+    {
+        return $this->render('pages/planifier-une-commande.html.twig');
+    }
+
+    /**
+     * @Route("/consultation", name="consultation-planning")
+     */
+    public function consultationAction()
+    {
+        return $this->render('pages/consulter-le-planning.html.twig');
     }
 }
