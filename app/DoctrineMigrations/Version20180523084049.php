b8442bc6bc35fc28cf61c497580c48cc4673faeb
benImran <benchaa.said-medjahed@hetic.net>
2018-05-27 15:40:18 +0200

    modif saledocumentline

---
+++ app/DoctrineMigrations/Version20180523084049.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180523084049 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_5727BE815EB52FD5 ON sale_document_line (documentId)');
+        $this->addSql('ALTER TABLE sale_document CHANGE status status VARCHAR(40) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sale_document CHANGE status status VARCHAR(40) DEFAULT \'Non planifié\' COLLATE utf8_unicode_ci');
+        $this->addSql('DROP INDEX UNIQ_5727BE815EB52FD5 ON sale_document_line');
+    }
+}
